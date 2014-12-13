30ce40ccd6271d827b081bfc7942f81b4dda4595
Michel Graciano <michel.graciano@gmail.com>
2014-12-13 18:54:41 -0200

    Versão inicial do archetype do Vert.x para a JM

---
+++ vertx-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.vertx.java.platform.Verticle;
+
+public class App extends Verticle {
+
+    @Override
+    public void start() {
+        //Seu código aqui
+    }
+}
